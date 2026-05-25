5bef677e9e719486cea71b5a1d3b73e6a910c7d4
Asadbek Yusupov <asadbekyusupov.info@gmail.com>
2026-05-25 22:14:46 +0500

    feat: .env configuration added for api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://task-manager-backend-laon.onrender.com/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
